fb59fcb6f80e8267775765a705fa1aa119438cfb
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-03-19 14:14:14 -0400

    Update getinfo.py

--- getinfo.py
+++ getinfo.py
@@ -5,6 +5,21 @@ app=Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 
 def main():
-    if(request.method == 'GET'):
-        return render_template('personinfo.html')
+    if(request.method == 'POST'):
+        
+        MainScreen()
+        return render_template("forloopintro.html", mylistlen=mylistlen, mylist=mylist)
     else:
+        return render_template("IndexFor.html")
+
+def MainScreen():
+    global mylist
+    global mylistlen
+
+    mylist=[]
+    mylist=["Math", "Science", "English", "History", "Software Engineering"]
+    mylistlen=len(mylist)
+    return mylistlen, mylist
+
+if __name__=="__main__":
+    app.run()
